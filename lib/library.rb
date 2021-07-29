57a7b170be143c245bd55e606c2a623006a29e97
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-29 13:47:36 -0700

    Add popular book and checkout tally methods to class

--- lib/library.rb
+++ lib/library.rb
@@ -1,10 +1,11 @@
 class Library
-  attr_reader :name, :books, :checked_out_books, :authors
+  attr_reader :name, :books, :checked_out_books, :check_out_tally, :authors
 
   def initialize(name)
     @name = name
     @books = []
     @checked_out_books = []
+    @check_out_tally = {}
     @authors = []
   end
 
@@ -32,6 +33,7 @@ class Library
     if @checked_out_books.include?(book) || !@books.include?(book)
       false
     else
+      book_checkout_tally(book)
       @checked_out_books << book
       @books.delete(book)
       true
@@ -41,5 +43,14 @@ class Library
   def return(book)
     @checked_out_books.delete(book)
     @books << book
+  end
+
+  def most_popular_book
+    @check_out_tally.key(@check_out_tally.values.max)
+  end
+
+  def book_checkout_tally(book)
+    @check_out_tally[book] ||= 0
+    @check_out_tally[book] += 1
   end
 end
