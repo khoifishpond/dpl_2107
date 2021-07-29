9a0cec7e1dc712224675f6410812d94c685d9e56
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-29 12:07:06 -0700

    Create book class

---
+++ lib/book.rb
@@ -0,0 +1,13 @@
+class Book
+  attr_reader :author_first_name,
+              :author_last_name,
+              :title,
+              :publication_date
+
+  def initialize(params)
+    @author_first_name = params[:author_first_name]
+    @author_last_name = params[:author_last_name]
+    @title = params[:title]
+    @publication_date = params[:publication_date]
+  end
+end
