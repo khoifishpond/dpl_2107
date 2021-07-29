e528c6104e17c3c3904fdbaa00abd598ea9f17ba
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-29 13:16:05 -0700

    Add publication methods to class

--- lib/library.rb
+++ lib/library.rb
@@ -13,4 +13,17 @@ class Library
       @books << book
     end
   end
+
+  def publication_time_frame_for(author)
+    publication_time_fame = {}
+    publication_time_fame[:start] = publication_years_for(author).sort.min
+    publication_time_fame[:end] = publication_years_for(author).sort.max
+    publication_time_fame
+  end
+
+  def publication_years_for(author)
+    author.books.map do |book|
+      book.publication_year
+    end
+  end
 end
