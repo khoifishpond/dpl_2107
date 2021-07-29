cff356196cd241313f5d474fa09891f992e072c0
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-29 12:34:28 -0700

    Add has a name to test

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -10,5 +10,9 @@ describe Library do
     it 'exists' do
       expect(@dpl).to be_a(Library)
     end
+
+    it 'has a name' do
+      expect(@dpl.name).to eq("Denver Public Library")
+    end
   end
 end
