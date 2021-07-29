c1bf603cee84212aaba56b316c0f22302baf552c
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-29 12:16:58 -0700

    Add books instance variable

--- lib/author.rb
+++ lib/author.rb
@@ -1,8 +1,13 @@
 class Author
-  attr_reader :first_name, :last_name
-  
+  attr_reader :first_name, :last_name, :books
+
   def initialize(params)
     @first_name = params[:first_name]
     @last_name = params[:last_name]
+    @books = []
+  end
+
+  def name
+    @first_name + " " + @last_name
   end
 end
