b43b84d382738bdcfaec614802097b578c0435a2
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-29 13:02:32 -0700

    Add missing } to hash

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -28,7 +28,7 @@ describe Library do
   context 'Behaviors' do
     before(:each) do
       @dpl = Library.new("Denver Public Library")
-      @charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte")
+      @charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
       @jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
       @professor = @charlotte_bronte.write("The Professor", "1857")
       @villette = @charlotte_bronte.write("Villette", "1853")
