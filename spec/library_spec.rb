4bd3e92adc6f175e15a80847109f3fc924015447
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-29 12:34:54 -0700

    Add starts with no books to test

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -14,5 +14,9 @@ describe Library do
     it 'has a name' do
       expect(@dpl.name).to eq("Denver Public Library")
     end
+
+    it 'starts with no books' do
+      expect(@dpl.books).to eq([])
+    end
   end
 end
