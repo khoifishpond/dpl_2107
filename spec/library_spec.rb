140b54da50982580c7289311252736173a450e97
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-29 12:33:17 -0700

    Change variable name to dpl

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -4,11 +4,11 @@ require 'rspec'
 describe Library do
   context 'Attributes' do
     before(:each) do
-      @library = Library.new("Denver Public Library")
+      @dpl = Library.new("Denver Public Library")
     end
     
     it 'exists' do
-      expect(@library).to be_a(Library)
+      expect(@dpl).to be_a(Library)
     end
   end
 end
